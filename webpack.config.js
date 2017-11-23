c13908f90f73a6afbf1dc25525d6ca8708eaa8f3
sudheer singh <justsudheerpaliwal@gmail.com>
2017-11-23 11:36:48 +0530

    add babel polyfill to support generators

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: './src/index.jsx',
+  entry: ['babel-polyfill', './src/index.jsx'],
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public/'),
